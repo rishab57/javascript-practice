e422da148b2892b7db2d3f37828994cedfe61692
Rishab57 <rishabkhurana.india@gmail.com>
2024-03-12 13:05:54 +0530

    updated

--- promise/promise.js
+++ promise/promise.js
@@ -95,5 +95,15 @@
 //         console.log('E:', error);
 //     }
 // }
-
 // getAllUsers()
+
+
+
+
+// fetch('http://jsonplaceholder.typicode.com/users')
+// .then((response)=>{
+//     return response.json()
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
